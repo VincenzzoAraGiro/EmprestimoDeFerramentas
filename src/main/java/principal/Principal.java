23c0ff9452c50815e28e89b675ed3ab086dbcb2c
Vincenzzo Aravechia Giro <vincenzzopsn2@gmail.com>
2024-05-29 11:04:18 -0300

    - Adicionado Principal

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,12 @@
+package principal;
+    
+import visao.frmMenuPrincipal;
+    
+    public class principal {
+        public static void main(String[] args) {
+    // Instancia a interface gráfica
+         frmMenuPrincipal objetotela = new frmMenuPrincipal();
+    // Torna a janela visível
+          objetotela.setVisible(true);
+        }
+    }
