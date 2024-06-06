42624b818c222ed5b37f1cebc7428c658dee4db3
Vincenzzo Aravechia Giro <vincenzzopsn2@gmail.com>
2024-06-06 00:35:01 -0300

    - Adicionado correção ao Emprestimo e EmprestimoDAO

--- src/main/java/modelo/Emprestimo.java
+++ src/main/java/modelo/Emprestimo.java
@@ -1,103 +1,60 @@
-import java.util.ArrayList;
+package modelo;
+
 import java.util.Date;
-import java.util.List;
-
-public class Ferramenta {
-    private String nome;
-    private String marca;
-    private double custo;
-
-    public Ferramenta(String nome, String marca, double custo) {
-        this.nome = nome;
-        this.marca = marca;
-        this.custo = custo;
-    }
-
-    // Getters and Setters
-}
-
-public class Amigo {
-    private String nome;
-    private String telefone;
-
-    public Amigo(String nome, String telefone) {
-        this.nome = nome;
-        this.telefone = telefone;
-    }
-
-    // Getters and Setters
-}
 
 public class Emprestimo {
-    private Ferramenta ferramenta;
-    private Amigo amigo;
+    private int idEmprestimo;
+    private int idAmigo;
+    private int idFerramenta;
     private Date dataEmprestimo;
     private Date dataDevolucao;
 
-    public Emprestimo(Ferramenta ferramenta, Amigo amigo, Date dataEmprestimo) {
-        this.ferramenta = ferramenta;
-        this.amigo = amigo;
+    public Emprestimo(int idEmprestimo, int idAmigo, int idFerramenta, Date dataEmprestimo, Date dataDevolucao) {
+        this.idEmprestimo = idEmprestimo;
+        this.idAmigo = idAmigo;
+        this.idFerramenta = idFerramenta;
         this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    // Getters e setters
+    public int getIdEmprestimo() {
+        return idEmprestimo;
+    }
+
+    public void setIdEmprestimo(int idEmprestimo) {
+        this.idEmprestimo = idEmprestimo;
+    }
+
+    public int getIdAmigo() {
+        return idAmigo;
+    }
+
+    public void setIdAmigo(int idAmigo) {
+        this.idAmigo = idAmigo;
+    }
+
+    public int getIdFerramenta() {
+        return idFerramenta;
+    }
+
+    public void setIdFerramenta(int idFerramenta) {
+        this.idFerramenta = idFerramenta;
+    }
+
+    public Date getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(Date dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
     }
 
     public void setDataDevolucao(Date dataDevolucao) {
         this.dataDevolucao = dataDevolucao;
-    }
-
-    // Getters e Setters
-}
-
-public class SistemaEmprestimo {
-    private List<Ferramenta> ferramentas;
-    private List<Amigo> amigos;
-    private List<Emprestimo> emprestimos;
-
-    public SistemaEmprestimo() {
-        ferramentas = new ArrayList<>();
-        amigos = new ArrayList<>();
-        emprestimos = new ArrayList<>();
-    }
-
-    public void cadastrarFerramenta(Ferramenta ferramenta) {
-        ferramentas.add(ferramenta);
-    }
-
-    public void cadastrarAmigo(Amigo amigo) {
-        amigos.add(amigo);
-    }
-
-    public void registrarEmprestimo(Ferramenta ferramenta, Amigo amigo, Date dataEmprestimo) {
-        Emprestimo emprestimo = new Emprestimo(ferramenta, amigo, dataEmprestimo);
-        emprestimos.add(emprestimo);
-    }
-
-    public void registrarDevolucao(Emprestimo emprestimo, Date dataDevolucao) {
-        emprestimo.setDataDevolucao(dataDevolucao);
-    }
-
-    // Outros métodos para gerar relatórios conforme as necessidades do seu tio-avô
-}
-
-public class Main {
-    public static void main(String[] args) {
-        SistemaEmprestimo sistema = new SistemaEmprestimo();
-
-        // Exemplo de cadastro de ferramentas e amigos
-        sistema.cadastrarFerramenta(new Ferramenta("Martelo", "Tramontina", 30.0));
-        sistema.cadastrarFerramenta(new Ferramenta("Chave de fenda", "Stanley", 20.0));
-
-        sistema.cadastrarAmigo(new Amigo("João", "123456789"));
-        sistema.cadastrarAmigo(new Amigo("Maria", "987654321"));
-
-        // Exemplo de registro de empréstimo
-        Ferramenta martelo = sistema.getFerramentas().get(0);
-        Amigo joao = sistema.getAmigos().get(0);
-        sistema.registrarEmprestimo(martelo, joao, new Date());
-
-        // Exemplo de registro de devolução
-        Emprestimo emprestimo = sistema.getEmprestimos().get(0);
-        sistema.registrarDevolucao(emprestimo, new Date());
-
-        // Implemente os métodos necessários para gerar os relatórios
     }
 }
